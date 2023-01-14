1ff75e05297c5a210775b390b94afb74e3dbb425
Dmitri Orlov <dmitri.orlov.anatolevich@gmail.com>
2023-01-14 13:28:46 +0600

    Добавлена возможность выбора количества слайдов.

--- js/main.js
+++ js/main.js
@@ -1,15 +1,58 @@
 "use strict";
 const BtnBack = document.querySelector(".slider-back");
 const BtnNext = document.querySelector(".slider-next");
+const SelectCountSlider = document.querySelector(".select-number");
+const SendSelect = document.querySelector(".send");
 const srcSlide = [];
+let classNumber = 0;
+let newSrc = [];
 let offset = 0;
+let countSlide = 1;
 const speedAnimation = 1;
+let currentSliderCount = [];
+
+
+
 
 CopySrc();
 CreateSlide();
+ReloadSlider();
 
 BtnNext.onclick = SlideNext;
 BtnBack.onclick = SlideBack;
+SendSelect.onclick = ReloadSlider;
+
+
+
+
+
+
+
+//Сбрасывает количество слайдеров
+function ReloadSlider() {
+  document.querySelector(".slide-single").remove();
+  CreateSlide();
+  classNumber = 0;
+  GetCurrentSliderCount()
+}
+function GetCurrentSliderCount() {
+  countSlide = Number(SelectCountSlider.value);
+  GetCountSlide(countSlide);
+  newSrc = [];
+  for (let i = 0; i < countSlide; i++) {
+    newSrc[i] = srcSlide[i];
+  }
+}
+function GetCountSlide(countSlide) {
+  currentSliderCount = [];
+  for (let i = 0; i < countSlide-1; i++) {
+    currentSliderCount.push(i);
+  }
+  currentSliderCount.unshift(countSlide-1);
+  currentSliderCount.unshift('');
+  console.log(currentSliderCount);
+}
+//............................
 
 //Копирует путь к изображениям
 function CopySrc() {
@@ -71,60 +114,77 @@ function TimerSlideBack() {
   //Если вперед то stepPosition = -5, currentPositionIndex1 = -530; stopIntervalPosition = -5;
 }
 
+// function GetClassNumberGeneric(direction) {
+//   let tempSlideHtml = document.querySelectorAll(".slide-single");
+//   let tempClassName = tempSlideHtml[0].className;
+//   let classNumber = parseInt(tempClassName.match(/\d+/));
+
+//   if (direction === "next") {
+//     if (classNumber === srcSlide.length) {
+//       classNumber = 0;
+//     }
+//     return classNumber;
+//   } else if (direction === "back") {
+//     switch (classNumber) {
+//       case 1:
+//         classNumber = 9;
+//         break;
+//       case 10:
+//         classNumber = 8;
+//         break;
+//       case 9:
+//         classNumber = 7;
+//         break;
+//       case 8:
+//         classNumber = 6;
+//         break;
+//       case 7:
+//         classNumber = 5;
+//         break;
+//       case 6:
+//         classNumber = 4;
+//         break;
+//       case 5:
+//         classNumber = 3;
+//         break;
+//       case 4:
+//         classNumber = 2;
+//         break;
+//       case 3:
+//         classNumber = 1;
+//         break;
+//       case 2:
+//         classNumber = 0;
+//         break;
+
+//       default:
+//         break;
+//     }
+
+//     return classNumber;
+//   } else {
+//     console.log("Ошибка тут");
+//   }
+// }
 function GetClassNumberGeneric(direction) {
   let tempSlideHtml = document.querySelectorAll(".slide-single");
   let tempClassName = tempSlideHtml[0].className;
-  let classNumber = parseInt(tempClassName.match(/\d+/));
+  classNumber = parseInt(tempClassName.match(/\d+/));
 
   if (direction === "next") {
-    if (classNumber === srcSlide.length) {
+    if (classNumber === newSrc.length) {
       classNumber = 0;
     }
     return classNumber;
   } else if (direction === "back") {
-    switch (classNumber) {
-      case 1:
-        classNumber = 9;
-        break;
-      case 10:
-        classNumber = 8;
-        break;
-      case 9:
-        classNumber = 7;
-        break;
-      case 8:
-        classNumber = 6;
-        break;
-      case 7:
-        classNumber = 5;
-        break;
-      case 6:
-        classNumber = 4;
-        break;
-      case 5:
-        classNumber = 3;
-        break;
-      case 4:
-        classNumber = 2;
-        break;
-      case 3:
-        classNumber = 1;
-        break;
-      case 2:
-        classNumber = 0;
-        break;
-
-      default:
-        break;
-    }
-
+    classNumber = currentSliderCount[classNumber];
     return classNumber;
   } else {
     console.log("Ошибка тут");
   }
 }
 function CreateSlideGeneric(classNumber, startPosition) {
-    offset = 1;
+  offset = 1;
   let img = document.createElement("img");
   img.src = srcSlide[classNumber];
   img.classList.add("slide-single");
